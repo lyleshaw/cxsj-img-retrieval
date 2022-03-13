048335febe94f809ebc1a6ee61e73cfc372153c8
xiaoliangyu <xiaoliangyu@bytedance.com>
2022-03-13 18:17:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -60,4 +60,4 @@ if __name__ == '__main__':
     model = load_model(use_gpu=True)
     import uvicorn
 
-    uvicorn.run(app, host="127.0.0.1", port=8181)
+    uvicorn.run(app, host="0.0.0.0", port=8181)
